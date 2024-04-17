f8b99fa99e103e0ba2bde8ee4daf484400cd4747
thomaschor <99774559+thomaschor@users.noreply.github.com>
2024-04-17 16:41:53 -0700

    Style

--- public/script.js
+++ public/script.js
@@ -3,3 +3,7 @@
 document.getElementById('page2Button').addEventListener('click', function() {
   window.location.href = 'teams_list.html';
 });
+
+document.getElementById('insertPageButton').addEventListener('click', function() {
+  window.location.href = 'insert_page.html';
+});
